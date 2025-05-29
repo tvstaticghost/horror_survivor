62ce5ec992ab480ee7f33e214c50a9d2cbcbc378
tvstaticghost <134819149+tvstaticghost@users.noreply.github.com>
2025-05-29 17:25:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html', horror_season=info.horror_survivor_season, starting_tribes=info.starting_tribes, predictions=info.predictions, shelter_events=info.shelter_events, starting_alliances=info.starting_alliances,
-                           immunity_challenges=info.immunity_challenges, immunity_challenge_winners=info.immunity_challenge_winners)
+                           immunity_challenges=info.immunity_challenges, immunity_challenge_winners=info.immunity_challenge_winners, tribal_councils=info.tribal_councils, tribal_vote_outs=info.tribal_vote_outs, random_events=info.random_events,
+                           final_votes=info.final_votes)
 
 if __name__ == '__main__':
     app.run(debug=True)
